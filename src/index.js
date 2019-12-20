a76f81ff0ef61417f4bcc941efd944d8da691960
matoeltiempo <matoeltiempoav@gmail.com>
2019-12-20 12:53:38 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './index.css';
+
+const config = {
+  type: "carousel",
+  perView: 3,
+  focusAt: "center",
+  startAt: 2,
+  gap: 16,
+  keyboard: true,
+  animationDuration: 700,
+  rewindDuration: 2000,
+  animationTimingFunc: 'ease-in-out',
+};
+
+new Glide('.glide', config).mount()
